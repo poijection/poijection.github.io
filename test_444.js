1faa0c2173d0a3ee2121f1368f66ab45245a7974
poijection <33039478+poijection@users.noreply.github.com>
2025-09-01 09:20:22 +0600

    Create test_444.js

---
+++ test_444.js
@@ -0,0 +1,14 @@
+const API_URL=window.config.apiBase+"/login";
+
+
+let base="/api/";
+let endpoint=base+"user";
+fetch(endpoint, { method: "GET" });
+
+
+const API_URL = window.config.apiBase + "/sssslogin";
+
+
+let base = "/ssssapi/";
+let endpoint = base + "ssssuser";
+fetch(endpoint, { method: "GET" });
